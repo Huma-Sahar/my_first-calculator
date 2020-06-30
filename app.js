30916b4dedcb4445c6e9ccde55d9c6a12f6f2ceb
Huma Sahar <humasahar308@gmail.com>
2020-06-30 22:46:46 +0500

    my first calculator app

---
+++ app.js
@@ -0,0 +1,22 @@
+function getnumber(num){
+    
+
+var result = document.getElementById("result");
+
+result.value += num;
+}
+function clearResult(){
+    var result = document.getElementById("result");
+
+    result.value = ("0");
+      
+}
+function getResult(){
+
+    var result = document.getElementById("result");
+
+    result.value = eval(result.value)
+      
+
+
+}
